fd6ccffaa529e0e63bd7916d6e9e3223288459a3
pamearteaga <pe_arteaga@hotmail.com>
2018-01-03 10:25:07 -0300

    comentarios mejorado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,31 @@
 $(document).ready(function(){
-      /*
-      * función para crear nuevo comentario
-      */
+
+
+    /*
+    * función para crear nuevo comentario
+    */
 
     $("#publicar").click(function(){
-        var comment = $("#comentario").val();
-        $("#comentario").val('');
-
-        $("#comentario-nuevo").append('<div class="texto">' + '<p>' + comment + '</p>' + '</div>');
-
-        if (comment == "") {
-          $("#publicar").attr("disabled","disabled")
-        }
+      var comment = $("#comentario").val();
+      $("#comentario").val('');
+      if (comment.length == 0 || comment == null) {
+         $("#publicar").attr("class","nulo")
+        return false;
+      }
+      $("#publicar").removeAttr("class")
+      $("#publicar").attr("class","activo")
+      $("#comentario-nuevo").prepend('<div class="texto">' + '<p>' + comment + '</p>' + '</div>');
         
-    });
-});
+    });// funcion publicar
+
+    $("#comentario").keydown(function(){
+      $("#publicar").removeAttr("class")
+      $("#publicar").attr("class","activo")
+    });// boton activo/nulo
+
+
+
+
+
+}); //document ready
+
