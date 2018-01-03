ece61f2ca0bcdcded9890f3e7639551820a2f69a
pamearteaga <pe_arteaga@hotmail.com>
2018-01-03 18:29:35 -0300

    comentarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,22 +8,28 @@ $(document).ready(function(){
     $("#publicar").click(function(){
       var comment = $("#comentario").val();
       $("#comentario").val('');
+      var media = $("#foto-video");
+
       if (comment.length == 0 || comment == null) {
          $("#publicar").attr("class","nulo")
         return false;
       }
       $("#publicar").removeAttr("class")
       $("#publicar").attr("class","activo")
-      $("#comentario-nuevo").prepend('<div class="texto">' + '<p>' + comment + '</p>' + '</div>');
-        
+      $("#comentario-nuevo").prepend('<div class="contenido">' + '<p>' + comment + '</p>' + 
+        '<img class="img-thumbnail img-responsive img-comentario" src="https://t1.uc.ltmcdn.com/images/6/1/6/img_por_que_mi_gato_maulla_mucho_26616_600.jpg">' + '</div>');
+        //'<img class="img-thumbnail img-responsive img-comentario" src="' + media + '">'
     });// funcion publicar
 
     $("#comentario").keydown(function(){
       $("#publicar").removeAttr("class")
       $("#publicar").attr("class","activo")
-    });// boton activo/nulo
+    });// boton para comentar activo/nulo
 
-
+ //evento que al hacer click le agrega color rojo al corazón
+    /*heart.addEventListener('click', function(){
+        heart.classList.toggle('red');
+    })*/
 
 
 
